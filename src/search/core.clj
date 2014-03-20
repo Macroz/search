9e26920f0893f96ac1e2cb09e9cbd1ee8edaaea1
Markku Rontu <markku.rontu@iki.fi>
2014-03-20 22:39:48 +0200

    Implement IDA* (probably doesn't work)

--- src/search/core.clj
+++ src/search/core.clj
@@ -39,3 +39,24 @@
   [start goal?-fn distance-fn neighbors-fn max-depth]
   ;; Dijkstra is a special case of A* when the heuristic is zero
   (a* start goal?-fn distance-fn (constantly 0) neighbors-fn max-depth))
+
+(defn ida*
+  "Calculate shortest path using IDA*."
+  [start goal?-fn distance-fn heuristic-fn neighbors-fn]
+  (let [ida nil
+        ida (fn [node path distance max-distance]
+              (let [new-distance (+ distance (heuristic-fn node))]
+                (cond (> new-distance max-distance) new-distance
+                      (goal?-fn node) path
+                      :else (loop [neighbors (neighbors-fn start)
+                                   min-distance nil]
+                              (let [neighbor (first neighbors)
+                                    result (ida ida neighbor (conj path neighbor) (+ distance (distance-fn node neighbor)) max-distance)]
+                                (cond (vector? result) result
+                                      (or (not min-distance)
+                                          (< result min-distance)) (recur (rest neighbors) result)
+                                          :else (recur (rest neighbors) min-distance)))))))]
+    (loop [max-distance 1]
+      (let [result (ida start [] 0 1)]
+        (if (vector? result) result
+            (recur result))))))
