072ae6f14c2e0fa75a3b2f102fc3b8bc3fa87d06
Markku Rontu <markku.rontu@iki.fi>
2014-03-21 08:44:11 +0200

    Split IDA* into two functions and fix the problems.

--- src/search/core.clj
+++ src/search/core.clj
@@ -40,23 +40,27 @@
   ;; Dijkstra is a special case of A* when the heuristic is zero
   (a* start goal?-fn distance-fn (constantly 0) neighbors-fn max-depth))
 
+(defn- ida
+  "Helper function for IDA* search."
+  [node path distance max-distance goal?-fn distance-fn heuristic-fn neighbors-fn]
+  (let [new-distance (+ distance (heuristic-fn node))]
+    (cond (> new-distance max-distance) new-distance
+          (goal?-fn node) path
+          :else (loop [neighbors (neighbors-fn node)
+                       min-distance nil]
+                  (if (empty? neighbors)
+                    min-distance
+                    (let [neighbor (first neighbors)
+                          result (ida neighbor (conj path neighbor) (+ distance (distance-fn node neighbor)) max-distance goal?-fn distance-fn heuristic-fn neighbors-fn)]
+                      (cond (vector? result) result
+                            (or (not min-distance)
+                                (< result min-distance)) (recur (rest neighbors) result)
+                                :else (recur (rest neighbors) min-distance))))))))
+
 (defn ida*
   "Calculate shortest path using IDA*."
   [start goal?-fn distance-fn heuristic-fn neighbors-fn]
-  (let [ida nil
-        ida (fn [node path distance max-distance]
-              (let [new-distance (+ distance (heuristic-fn node))]
-                (cond (> new-distance max-distance) new-distance
-                      (goal?-fn node) path
-                      :else (loop [neighbors (neighbors-fn start)
-                                   min-distance nil]
-                              (let [neighbor (first neighbors)
-                                    result (ida ida neighbor (conj path neighbor) (+ distance (distance-fn node neighbor)) max-distance)]
-                                (cond (vector? result) result
-                                      (or (not min-distance)
-                                          (< result min-distance)) (recur (rest neighbors) result)
-                                          :else (recur (rest neighbors) min-distance)))))))]
-    (loop [max-distance 1]
-      (let [result (ida start [] 0 1)]
-        (if (vector? result) result
-            (recur result))))))
+  (loop [max-distance 1]
+    (let [result (ida start [start] 0 max-distance goal?-fn distance-fn heuristic-fn neighbors-fn)]
+      (if (vector? result) result
+          (recur result)))))
