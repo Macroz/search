edf3caf7eebb0df4c02b850817ca8a4adc0a2cab
Markku Rontu <markku.rontu@iki.fi>
2014-03-26 16:14:28 +0200

    Improve docstrings

--- src/search/core.clj
+++ src/search/core.clj
@@ -10,7 +10,7 @@
   start is the first node.
   goal?-fn is called for each expanded node to check whether it is a goal
   distance-fn is called with from and to nodes and should return a numeric distance.
-  heuristic-fn is called with a node should return a numeric estimated distance to the nearest goal.
+  heuristic-fn is called with a node and should return a numeric estimated distance to the nearest goal.
   neighbors-fn is called with the node and should return all the neighbors
   max-depth is the maximum depth of the path in steps."
   [start goal?-fn distance-fn heuristic-fn neighbors-fn max-depth]
@@ -60,7 +60,17 @@
                                 :else (recur (rest neighbors) min-distance))))))))
 
 (defn ida*
-  "Calculate shortest path using IDA*."
+  "Calculate shortest path using IDA*.
+
+  Note the heuristic function must be monotonous, increasing and never overestimate
+  (i.e. it must be admissible) to guarantee an optimal result.
+
+  start is the first node.
+  goal?-fn is called for each expanded node to check whether it is a goal
+  distance-fn is called with from and to nodes and should return a numeric distance.
+  heuristic-fn is called with a node and should return a numeric estimated distance to the nearest goal.
+  neighbors-fn is called with the node and should return all the neighbors
+  max-depth is the maximum depth of the path in steps."
   [start goal?-fn distance-fn heuristic-fn neighbors-fn max-depth]
   (loop [max-distance 1]
     (let [result (ida start [start] 0 max-distance goal?-fn distance-fn heuristic-fn neighbors-fn max-depth)]
